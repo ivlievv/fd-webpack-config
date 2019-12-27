111bc98a1c1c91109862a6916c3c8683c153b0d8
User Name <artur.it2914@gmail.com>
2019-12-27 16:41:22 +0200

    Add: json copying

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,10 +1,17 @@
 import styles from '../css/style.scss';
-import test from  '../../style.css';
 const greeting = document.createElement( 'h1' );
+
+fetch('employees.json')
+  .then(response => {
+    return response.json()
+  })
+  .then(console.log)
+  .catch(console.error);
+
 
 greeting.innerText = 'Test test test';
 
-greeting.classList.add( styles.test, test.class );
+greeting.classList.add( styles.test );
 
 document.body.appendChild( greeting );
 
